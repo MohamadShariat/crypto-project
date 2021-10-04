c2e29c99828ce9e919ead1211d5687d3fab7b6e0
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-10-04 23:05:57 +0330

    the loader file is created

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,17 +3,35 @@ import React, { useState, useEffect } from "react";
 //* API
 import { getCoin } from "../services/api";
 
+//* Components
+import Loader from "./Loader";
+
 const Landing = () => {
+  const [coins, setCoins] = useState([]);
+
   useEffect(() => {
     const fetchAPI = async () => {
       const data = await getCoin();
-      console.log(data);
+      setCoins(data);
     };
 
     fetchAPI();
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <input type="text" placeholder="Search" />
+      {coins.length ? (
+        <div>
+          {coins.map((coin) => (
+            <p key={coin.id}>{coin.name}</p>
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
 };
 
 export default Landing;
