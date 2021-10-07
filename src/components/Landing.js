a42cb8ffbdc33a4884bd1db4fa28ba05a458b2f2
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-10-07 13:44:35 +0330

    the cion file is created and search fields is coded

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,9 +5,11 @@ import { getCoin } from "../services/api";
 
 //* Components
 import Loader from "./Loader";
+import Coin from "./Coin";
 
 const Landing = () => {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -18,13 +20,34 @@ const Landing = () => {
     fetchAPI();
   }, []);
 
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <>
-      <input type="text" placeholder="Search" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={searchHandler}
+      />
       {coins.length ? (
         <div>
-          {coins.map((coin) => (
-            <p key={coin.id}>{coin.name}</p>
+          {searchCoins.map((coin) => (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              marketCap={coin.market_cap}
+              pricechange={coin.price_change_percentage_24h}
+            />
           ))}
         </div>
       ) : (
